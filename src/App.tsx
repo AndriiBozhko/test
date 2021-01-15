6157afef2bdc7d479e518809f41c0680ae486208
Andriy Bozhko <andriybozhko@MacBook-Pro-Andriy.local>
2021-01-15 16:46:41 +0200

    for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    var deferredPrompt: any;
+    const btnSave: any = document.querySelector('.add-button');
+
+window.addEventListener('beforeinstallprompt', function(e: any) {
+  console.log('beforeinstallprompt Event fired');
+  e.preventDefault();
+
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+
+  return false;
+});
+
+btnSave.addEventListener('click', function() {
+  if(deferredPrompt !== undefined) {
+    // The user has had a postive interaction with our app and Chrome
+    // has tried to prompt previously, so let's show the prompt.
+    deferredPrompt.prompt();
+
+    // Follow what the user has done with the prompt.
+    deferredPrompt.userChoice.then(function(choiceResult: any) {
+
+      console.log(choiceResult.outcome);
+
+      if(choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled home screen install');
+      }
+      else {
+        console.log('User added to home screen');
+      }
+
+      // We no longer need the prompt.  Clear it up.
+      deferredPrompt = null;
+    });
+  }
+});
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       
+        <button className="add-button">Add to home screen</button>
+
       </header>
     </div>
   );
